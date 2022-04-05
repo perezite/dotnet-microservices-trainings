0dbe76395e1bf3c0d6f429ea59da89b6e01526b0
Prez <simulo2000@gmail.com>
2022-04-05 22:45:30 +0200

    Added basic app

---
+++ building-debuggin-mservices/Demo/WebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            ViewData["Message"] = "Hello from Web Frontend";
+
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage();
+                // request.RequestUri = new Uri("https://localhost:55176/weatherforecast");
+                // request.RequestUri = new Uri("http://api/WeatherForecast");
+                // request.RequestUri = new Uri("https://localhost:55052/weatherforecast");
+                // var response = await client.SendAsync(request);
+                // ViewData["Message"] += " and " + await response.Content.ReadAsStringAsync();
+                ViewData["Message"] = " test test";
+            }
+        }
+    }
+}
--- building-debuggin-mservices/Readme .md
+++ building-debuggin-mservices/Readme .md
@@ -1,1 +1,2 @@
 Source: https://www.youtube.com/watch?v=98nIvg7ne7Q&ab_channel=dotNET
+Next: 15:40
