b706a9d6651c0db7cbcfdd4a6bea6d8c9c7a32cc
Prez <simulo2000@gmail.com>
2022-04-09 20:58:23 +0200

    Added api-collection

---
+++ api-collection/ApiCollection/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while(true)
+{
+    Console.WriteLine("Update");
+    Thread.Sleep(500);
+    using var httpClient = new HttpClient();
+    var api1Response = await httpClient.GetAsync("http://localhost:5090/weatherforecast");
+    var api2Response = await httpClient.GetAsync("http://localhost:5172/weatherforecast");
+
+    var message1 = await api1Response.Content.ReadAsStringAsync();
+    var message2 = await api2Response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(message1.Substring(0, 100) + "...");
+    Console.WriteLine(message1.Substring(0, 200) + "...");
+
+}
