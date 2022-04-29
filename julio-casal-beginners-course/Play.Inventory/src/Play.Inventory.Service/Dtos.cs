517de7682056423c0e880f043d7b79f71aaf818f
Prez <simulo2000@gmail.com>
2022-04-29 16:21:20 +0200

    Added catalog http client in inventory service

--- julio-casal-beginners-course/Play.Inventory/src/Play.Inventory.Service/Dtos.cs
+++ julio-casal-beginners-course/Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -4,5 +4,7 @@ namespace Play.Inventory.Service.Dtos
 {
     public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
 
-    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AquiredDate);
+    public record InventoryItemDto(Guid CatalogItemId, string Name, string Description, int Quantity, DateTimeOffset AquiredDate);
+
+    public record CatalogItemDto(Guid Id, string Name, string Description);
 }
